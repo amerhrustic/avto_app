2855963745e0f31fb120d890ef973d01fd1f1251
amerhrustic <amer.hrustic@scv.si>
2025-03-23 13:44:03 +0000

    kozda3

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -17,30 +17,30 @@ class CarsController < ApplicationController
     @models = [] # Prazna izbira modelov, dokler uporabnik ne izbere znamke
   end
 
-  # Ustvari nov avto v bazi
   def create
     @car = Car.new(car_params)
-
+  
     if params[:car][:model_name].present?
       # Če uporabnik vnese ime modela, ga ustvarimo in dodelimo avtomobilu
       model = Model.find_or_create_by(name: params[:car][:model_name], brand_id: params[:car][:brand_id])
       @car.model = model
     end
-
+  
     if @car.save
       # Dodaj slike, če so bile naložene
       if params[:car][:images].present?
         params[:car][:images].each do |image_file|
-          Image.create(url: image_file.url, car_id: @car.id)  # Prepričajte se, da image_file vsebuje URL
+          @car.images.attach(image_file) # Uporabi ActiveStorage za nalaganje slik
         end
       end
-
+  
       redirect_to cars_path, notice: 'Car successfully added!'
     else
       @models = Model.where(brand_id: params[:car][:brand_id]) # Naloži modele izbrane znamke
       render :new, status: :unprocessable_entity
     end
   end
+  
 
   # Prikaži obrazec za urejanje avtomobila
   def edit
