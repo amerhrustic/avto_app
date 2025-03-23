99a94d4c3d32f91a1c78d82c95bc027024a39fe9
amerhrustic <amer.hrustic@scv.si>
2025-03-23 12:18:43 +0000

    mozda

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::Base
-  helper_method :logged_in?, :current_user
-
-  def logged_in?
-    !!session[:user_id]
-  end
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if logged_in?
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ApplicationRecord
     has_secure_password
-  
-    # Dodajemo lahko še nekaj validacij, če želiš
-    validates :email, presence: true, uniqueness: true
   end
   
