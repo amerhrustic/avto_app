b53e12c62cbf98e63ed706de42ef818304b49de8
amerhrustic <amer.hrustic@scv.si>
2025-03-23 15:16:38 +0000

    OJJ

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,6 @@
 class Car < ApplicationRecord
   belongs_to :brand
   belongs_to :model  # Prej je bilo :car_model
-  has_many :images # Povezava s tabelo `images`
+  has_many_attached :images # Povezava s tabelo `images`
   validates :brand, :model, presence: true 
 end
