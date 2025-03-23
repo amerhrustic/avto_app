d8861c4b283c8164c5a73078ceb495723fb67a85
amerhrustic <amer.hrustic@scv.si>
2025-03-23 13:29:56 +0000

    mozda

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :brand
   belongs_to :model  # Prej je bilo :car_model
-  has_many :images, dependent: :destroy  # Povezava z več slikami
+  has_one_attached :car_image
+  has_many_attached :car_images
   validates :brand, :model, presence: true 
 end
