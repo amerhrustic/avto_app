65f8e7224b55f9746bea932f34581bc1abc41383
amerhrustic <amer.hrustic@scv.si>
2025-03-23 15:07:15 +0000

    srory

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,6 @@
 class Car < ApplicationRecord
   belongs_to :brand
   belongs_to :model  # Prej je bilo :car_model
-  has_many :images, dependent: :destroy  # Povezava s tabelo `images`
+  has_many :images # Povezava s tabelo `images`
   validates :brand, :model, presence: true 
 end
