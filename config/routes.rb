1b41b7d3e51b97f20fc9c54d9b88726068a196bb
amerhrustic <amer.hrustic@scv.si>
2025-03-26 00:50:09 +0100

    addasd

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   # Routes for Sessions (Login and Logout)
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+  delete "/logout", to: "sessions#destroy" 
 
   # Routes for Users (Registration)
   get "/register", to: "users#new"  # Registration form
